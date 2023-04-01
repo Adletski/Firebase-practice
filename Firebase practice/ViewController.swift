cfbef3a8b470a714f937203d2eea8a6a31c6c064
Adlet <adlet.zh19966@gmail.com>
2023-04-01 18:19:16 +0600

    Initial Commit

---
+++ Firebase practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase practice
+//
+//  Created by Adlet Zhantassov on 01.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
